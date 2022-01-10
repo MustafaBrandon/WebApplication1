e8225cf8f538e36a563004d1a684e469e98f4f79
Brandon <brandonjmhunt@gmail.com>
2022-01-09 21:45:27 -0700

    Initial code commit

---
+++ WebApplication1/wwwroot/js/fanmail.js
@@ -0,0 +1,8 @@
+﻿$("#buttonSend").addEventListener("click", function () {
+    alert("Message from " + $("#from").val());
+})
+
+document.getElementById("picture").addEventListener('click',function () {
+    alert("ayyyo");
+    //$("#picture").fadeToggle();
+})
